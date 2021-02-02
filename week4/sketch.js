d147c5e2f31f420c650425136513052d21f05aab
Mary Primeau <mp144297@umconnect.umt.edu>
2021-02-02 11:35:30 -0800

    made cat run ainamte

--- week4/sketch.js
+++ week4/sketch.js
@@ -1,61 +1,82 @@
-var dino;
+var cat;
 var animation = [];
 var runanimation = [];
-var dinoObjects = [];
-var dinoRunObjects = [];
+var catObjects = [];
+var catRunObjects = [];
+var catSlideObjects = [];
 var i = 0;
 var j = 0;
 var x = 0;
 var counter = 0;
 
 function preload(){
-  dino = new imageclass('assets/Idle (1).png', 0, 0);
-  dinoObjects[0] = dino;
-  dino = new imageclass('assets/Idle (2).png', 0, 0);
-  dinoObjects[1] = dino;
-  dino = new imageclass('assets/Idle (3).png', 0, 0);
-  dinoObjects[2] = dino;
-  dino = new imageclass('assets/Idle (4).png', 0, 0);
-  dinoObjects[3] = dino;
-  dino = new imageclass('assets/Idle (5).png', 0, 0);
-  dinoObjects[4] = dino;
-  dino = new imageclass('assets/Idle (6).png', 0, 0);
-  dinoObjects[5] = dino;
-  dino = new imageclass('assets/Idle (7).png', 0, 0);
-  dinoObjects[6] = dino;
-  dino = new imageclass('assets/Idle (8).png', 0, 0);
-  dinoObjects[7] = dino;
-  dino = new imageclass('assets/Idle (9).png', 0, 0);
-  dinoObjects[8] = dino;
-  dino = new imageclass('assets/Idle (10).png', 0, 0);
-  dinoObjects[9] = dino;
+  cat = new imageclass('assets/Idle (1).png', 0, 0);
+  catObjects[0] = cat;
+  cat = new imageclass('assets/Idle (2).png', 0, 0);
+  catObjects[1] = cat;
+  cat = new imageclass('assets/Idle (3).png', 0, 0);
+  catObjects[2] = cat;
+  cat = new imageclass('assets/Idle (4).png', 0, 0);
+  catObjects[3] = cat;
+  cat = new imageclass('assets/Idle (5).png', 0, 0);
+  catObjects[4] = cat;
+  cat = new imageclass('assets/Idle (6).png', 0, 0);
+  catObjects[5] = cat;
+  cat = new imageclass('assets/Idle (7).png', 0, 0);
+  catObjects[6] = cat;
+  cat = new imageclass('assets/Idle (8).png', 0, 0);
+  catObjects[7] = cat;
+  cat = new imageclass('assets/Idle (9).png', 0, 0);
+  catObjects[8] = cat;
+  cat = new imageclass('assets/Idle (10).png', 0, 0);
+  catObjects[9] = cat;
 
-  dino = new imageclass('assets/Run (1).png', 0, 0);
-  dinoRunObjects[0] = dino;
-  dino = new imageclass('assets/Run (2).png', 0, 0);
-  dinoRunObjects[1] = dino;
-  dino = new imageclass('assets/Run (3).png', 0, 0);
-  dinoRunObjects[2] = dino;
-  dino = new imageclass('assets/Run (4).png', 0, 0);
-  dinoRunObjects[3] = dino;
-  dino = new imageclass('assets/Run (5).png', 0, 0);
-  dinoRunObjects[4] = dino;
-  dino = new imageclass('assets/Run (6).png', 0, 0);
-  dinoRunObjects[5] = dino;
-  dino = new imageclass('assets/Run (7).png', 0, 0);
-  dinoRunObjects[6] = dino;
-  dino = new imageclass('assets/Run (8).png', 0, 0);
-  dinoRunObjects[7] = dino;
+  cat = new imageclass('assets/Run (1).png', 0, 0);
+  catRunObjects[0] = cat;
+  cat = new imageclass('assets/Run (2).png', 0, 0);
+  catRunObjects[1] = cat;
+  cat = new imageclass('assets/Run (3).png', 0, 0);
+  catRunObjects[2] = cat;
+  cat = new imageclass('assets/Run (4).png', 0, 0);
+  catRunObjects[3] = cat;
+  cat = new imageclass('assets/Run (5).png', 0, 0);
+  catRunObjects[4] = cat;
+  cat = new imageclass('assets/Run (6).png', 0, 0);
+  catRunObjects[5] = cat;
+  cat = new imageclass('assets/Run (7).png', 0, 0);
+  catRunObjects[6] = cat;
+  cat = new imageclass('assets/Run (8).png', 0, 0);
+  catRunObjects[7] = cat;
 
+  cat = new imageclass('assets/Slide (1).png', 0, 0);
+  catSlideObjects[0] = cat;
+  cat = new imageclass('assets/Slide (2).png', 0, 0);
+  catSlideObjects[1] = cat;
+  cat = new imageclass('assets/Slide (3).png', 0, 0);
+  catSlideObjects[2] = cat;
+  cat = new imageclass('assets/Slide (4).png', 0, 0);
+  catSlideObjects[3] = cat;
+  cat = new imageclass('assets/Slide (5).png', 0, 0);
+  catSlideObjects[4] = cat;
+  cat = new imageclass('assets/Slide (6).png', 0, 0);
+  catSlideObjects[5] = cat;
+  cat = new imageclass('assets/Slide (7).png', 0, 0);
+  catSlideObjects[6] = cat;
+  cat = new imageclass('assets/Slide (8).png', 0, 0);
+  catSlideObjects[7] = cat;
+  cat = new imageclass('assets/Slide (9).png', 0, 0);
+  catSlideObjects[8] = cat;
+  cat = new imageclass('assets/Slide (10).png', 0, 0);
+  catSlideObjects[9] = cat;
 
-  for (var i = 0; i < dinoObjects.length; i++) {
-      animation[i] = dinoObjects[i].getImage();
+  for (var i = 0; i < catObjects.length; i++) {
+      animation[i] = catObjects[i].getImage();
 
   }
 
-  for(var i = 0; i < dinoRunObjects.length; i++)
+  for(var i = 0; i < catRunObjects.length; i++)
   {
-    runanimation[i] = dinoRunObjects[i].getImage();
+    runanimation[i] = catRunObjects[i].getImage();
   }
 
 }
@@ -76,7 +97,7 @@ function draw() {
     if(key == "d")
         {
 // continue to display the image
-    image(runanimation[j], dinoRunObjects[j].getX(), dinoRunObjects[j].getY());
+    image(runanimation[j], catRunObjects[j].getX(), catRunObjects[j].getY());
 // this section slows the running down
     counter++;
     if(counter > 5)
@@ -90,7 +111,7 @@ function draw() {
     }
     else
     {
-        image(animation[i], dinoObjects[i].getX(), dinoObjects[i].getY());
+        image(animation[i], catObjects[i].getX(), catObjects[i].getY());
     }
 
 

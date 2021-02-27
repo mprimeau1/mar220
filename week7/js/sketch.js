b4b0c41f2ac499bff882a368dbb4cb45435a2350
Mary Primeau <mp144297@umconnect.umt.edu>
2021-02-27 14:32:07 -0800

    created particle stuff

--- week7/js/sketch.js
+++ week7/js/sketch.js
@@ -1,15 +1,13 @@
 var catObjects;
-var result, runresult, runLeftresult, fallresult, jumpresult, attackresult;
+var result, runresult, runLeftresult, attackresult;
 var crystal1;
-var crystal1X;
-var crystal1Y;
+const particles = [];
+var health = 100;
 
 function preload() {
   result = loadStrings('assets/characteridle.txt');
   runresult = loadStrings('assets/characterrun.txt');
   runLeftresult = loadStrings ('assets/characterrunleft.txt');
-  fallresult = loadStrings ('assets/characterfall.txt');
-  jumpresult = loadStrings ('assets/characterjump.txt');
   attackresult = loadStrings ('assets/characterattack.txt');
 }
 
@@ -19,8 +17,6 @@ function setup() {
     catObjects.addAnimation('idle', result[0], result[result.length-1]);
     catObjects.addAnimation('run', runresult[0], runresult[runresult.length-1]);
     catObjects.addAnimation('runLeft', runLeftresult[0], runLeftresult[runLeftresult.length-1]);
-    catObjects.addAnimation('fall', fallresult[0], fallresult[fallresult.length-1]);
-    catObjects.addAnimation('jump', jumpresult[0], jumpresult[jumpresult.length-1]);
     catObjects.addAnimation('attack', attackresult[0], attackresult[attackresult.length-1]);
     
     
@@ -37,61 +33,66 @@ function draw()
     {
       catObjects.changeAnimation('run');
       catObjects.velocity.x += .5;
+      if (crystal1 != null)
+        {
       if(catObjects.collide(crystal1))
       {
         catObjects.changeAnimation('idle');
       }
-    }
+    }}
     
     else if (keyDown('a'))
     {
       catObjects.changeAnimation('runLeft');
       catObjects.velocity.x -= .5;
-      if(catObjects.collide(crystal1))      
+      if (crystal1 != null)
       {
-        catObjects.changeAnimation('idle');
-      }
-      
-    }
-    
-    else if (keyDown('s'))
-    {
-      catObjects.changeAnimation('fall');
-      catObjects.velocity.y += .5;
-      if(catObjects.collide(crystal1))    
+      if(catObjects.collide(crystal1))      
       {
         catObjects.changeAnimation('idle');
       }  
     }
-    
-    else if (keyDown('w'))
-    {
-      catObjects.changeAnimation('jump');
-      catObjects.velocity.y -= .5;
-      if(catObjects.collide(crystal1))
-      {
-        catObjects.changeAnimation('idle');
-      }
-    }
+  }
     
     else if (keyDown('x'))
     {
       catObjects.changeAnimation('attack');
-      catObjects.velocity.x -= .5;
-      if(catObjects.collide(crystal1))
+      if (crystal1 != null)
       {
-        catObjects.changeAnimation('idle');
+        if(dist(catObjects.position.x,catObjects.position.y,crystal1.position.x,crystal1.position.y) < 250)
+        {
+          createParticles(crystal1.position.x, crystal1.position.y);
+          health -= 1;
+          if(health <= 0)
+          {
+            crystal1.remove();
+            crystal1 = null;
       }
     }
-    
+    }
+  }
     else
     {
       catObjects.changeAnimation('idle');
       catObjects.velocity.x = 0;
-      catObjects.velocity.y = 0;
     }
 
     catObjects.debug = mouseIsPressed;
-    crystal1.debug = mouseIsPressed;
     drawSprites();
+}
+
+function createParticles(x,y)
+{
+for (let i = 0; i < 5; i++) {
+    let p = new Particle(x,y);
+    particles.push(p);
+  }
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].show();
+    if (particles[i].finished()) {
+      // remove this particle
+      particles.splice(i, 1);
+    }
+  }
 }

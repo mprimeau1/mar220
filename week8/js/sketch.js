a70326c376f593fc1ce77d30e5347e39b1b27158
Mary Primeau <mp144297@umconnect.umt.edu>
2021-03-03 16:23:48 -0800

    finished week8

--- week8/js/sketch.js
+++ week8/js/sketch.js
@@ -1,51 +1,60 @@
 var x = 0;
 var y = 0;
 
+
 function setup() {
   createCanvas(800, 600, WEBGL);
 }
 
 // display all the frames using the draw function as a loop
 function draw() {
-  background(120);
+  background('black');
   normalMaterial();
 
-  createBox(150, 150, 150, -100, -100, true);
   createTorus();
-  createCone(50,150,24,24,true);
-  createSphere(150);
-  createBox(50, 50, 50, 250, 50, false);
+  createTorus();
+  createCone(25,200,24,24,true);
+  createSphere();
+  createBox(25, 150, 25, -100, -100, true);
+  
   if (keyIsPressed) {
     if (key == 'd') { // move the right
-      x += 2;}
+      x += 5;
+      }
 
    else if (key == 'a') { // move to the left
 
-      x -= 2;}
+      x -= 1;
+      }
     
     else if (key == 'w') { // move up 
 
-      y += 2;}
+      y += 5;
+      }
 
     else if (key == 's') { // move down 
 
-      y -= 2;}
+      y -= 1;
+      }
 
 }
 }
 
-// this allows us to create any type of box
 function createBox(w, h, d, transX, transY, move) {
   push();
   if (move) {
-    translate(transX + x, transY);
+    translate(transX + x, transY)
+    scale(2,1);
   } else {
     translate(transX, transY);
+    scale(0,0);
   }
   if (move) {
     translate(x, transY - y);
+    scale(-2,-1);
   } else {
     translate(transX, transY);
+    scale(0,0);
   }
 
   rotateX(frameCount * 0.01);
@@ -53,38 +62,49 @@ function createBox(w, h, d, transX, transY, move) {
   box(w, h, d);
   pop();
 
-
 }
 
-// this creates a single torus
 function createTorus() {
   push();
-  translate(100, 100);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  torus(50, 15, 24, 16);
+  translate(150, 150);
+  rotateX(frameCount * 0.2);
+  rotateY(frameCount * 0.2);
+  torus(25, 10, 24, 16);
+  pop();
+  
+  push();
+  translate(150, 150);
+  rotateX(frameCount * 0.1);
+  rotateY(frameCount * 0.1);
+  torus(100, 20, 24, 16);
   pop();
   
 }
-  
-// this creates a single cone  
+   
 
 function createCone() {
   push();
-  translate(75, -50);
+  translate(200, -100);
   rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  cone(50, 150, 24, 24,true);
+  rotateY(frameCount * 0.1);
+  cone(25, 200, 24, 24,true);
   pop();
   
 }
 
 function createSphere() {
   push();
-  translate(-125, 125);
+  translate(-200, 125);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
   sphere(50);
+  pop();
+  
+  push();
+  translate(150, 150);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  sphere(20);
   pop();
   
 }
